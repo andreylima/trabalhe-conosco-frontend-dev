e72c4d12b3364cbc6be07410da352a590d0850b1
andreylima <andreylrgv@hotmail.com>
2018-11-14 10:00:18 -0200

    foms sitepoint

---
+++ bank-app/src/app/transfer/transfer.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { BankAppService } from '../bank-app.service';
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'app-transfer',
+  templateUrl: './transfer.component.html',
+  styleUrls: ['./transfer.component.css']
+})
+export class TransferComponent implements OnInit {
+
+  constructor(private atService: BankAppService,private router: Router) { }
+
+  userData : any = {};
+  cachedUserData : any;
+  hasCardRegistered : boolean = false;
+  user : any;
+  finalNumber : number;
+
+  chooseCard()
+  {
+  	this.router.navigate(['/choose-card']);
+  }
+
+  registerCard()
+  {
+  	this.router.navigate(['/register-card']);
+  }
+
+  ngOnInit() {
+  	this.user = this.atService.getUser();
+  	this.cachedUserData = localStorage.getItem("mycards");
+  	if (this.cachedUserData != null) {
+  		this.hasCardRegistered  = true;
+  		this.cachedUserData = this.cachedUserData[0];
+  		console.log(this.cachedUserData);
+  	}
+  }
+
+}
